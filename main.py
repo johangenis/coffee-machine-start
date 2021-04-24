1b63db40fbb1b2b0864c7f30f4ad4d4406f39519
johangenis <johan.j.genis@gmail.com>
2021-04-24 16:29:04 +0200

    Added money dictionary to data.py. Added format_data(resources, money) function to format resources and money into a printable format to produce a report on the quantities of: water, milk, coffee and money.

--- main.py
+++ main.py
@@ -1,10 +1,41 @@
-from data import menu, resources
+from data import menu, resources, money
 
-# TODO: 1 Prompt user by asking "What would you like? (espresso/latte/cappuccino):"
+machine_on = True
 
-# TODO: 2 Turn off the Coffee Machine by entering "off" at the prompt.
+while machine_on:
 
-# TODO: 3 Print report.
+    def format_data(resources, money):
+        """
+        Format resources and money into a printable format to produce a
+        report on the quantities of: water, milk, coffee and money.
+        """
+
+        water = resources["water"]
+        milk = resources["milk"]
+        coffee = resources["coffee"]
+        quarters_amount = (
+            money["quarters"]["value"] * money["quarters"]["count"]
+        )
+        dimes_amount = money["dimes"]["value"] * money["dimes"]["count"]
+        nickles_amount = money["nickles"]["value"] * money["nickles"]["count"]
+        pennies_amount = money["pennies"]["value"] * money["pennies"]["count"]
+        total_amount = (
+            quarters_amount + dimes_amount + nickles_amount + pennies_amount
+        )
+        return f"Water: {water}ml\nMilk: {milk}\nCoffee: {coffee}\nMoney: ${total_amount}"
+
+    # TODO: 1 Prompt user by asking "What would you like? (espresso/latte/cappuccino):"
+    user_choice = input(
+        "What would you like? (espresso/latte/cappuccino):"
+    ).lower()
+
+    # 2. Turn off the Coffee Machine by entering "off" at the prompt.
+    if user_choice == "off":
+        print(f"Machine was turned off.")
+        machine_on = False
+    # 3. Print report.
+    elif user_choice == "report":
+        print(format_data(resources, money))
 
 # TODO: 4 Check resources sufficient?
 
