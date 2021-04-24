e51c9cf42d73a3cd9b2d1d7c4d8cf5d939c1d911
johangenis <johan.j.genis@gmail.com>
2021-04-24 14:56:01 +0200

    Planned Coffee Machine app by inserting 7 TODO's

--- main.py
+++ main.py
@@ -1,32 +1,15 @@
-MENU = {
-    "espresso": {
-        "ingredients": {
-            "water": 50,
-            "coffee": 18,
-        },
-        "cost": 1.5,
-    },
-    "latte": {
-        "ingredients": {
-            "water": 200,
-            "milk": 150,
-            "coffee": 24,
-        },
-        "cost": 2.5,
-    },
-    "cappuccino": {
-        "ingredients": {
-            "water": 250,
-            "milk": 100,
-            "coffee": 24,
-        },
-        "cost": 3.0,
-    }
-}
+from data import menu, resources
 
-resources = {
-    "water": 300,
-    "milk": 200,
-    "coffee": 100,
-}
+# TODO: 1 Prompt user by asking "What would you like? (espresso/latte/cappuccino):"
 
+# TODO: 2 Turn off the Coffee Machine by entering "off" at the prompt.
+
+# TODO: 3 Print report.
+
+# TODO: 4 Check resources sufficient?
+
+# TODO: 5 Process coins.
+
+# TODO: 6 Check transaction successful?
+
+# TODO: 7 Make Coffee.
