eafbe9b015f6604ea6eaccd6622c9d8ec56a70a2
johangenis <johan.j.genis@gmail.com>
2021-04-26 18:57:16 +0200

    Started work on check resources - TODO: 4

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from data import menu, resources, money
 
 machine_on = True
@@ -25,6 +24,37 @@ while machine_on:
         )
         return f"Water: {water}ml\nMilk: {milk}\nCoffee: {coffee}\nMoney: ${total_amount}"
 
+    def check_resources(user_choice):
+        choice = ""
+        espresso = menu["espresso"]
+        latte = menu["latte"]
+        cappuccino = menu["cappuccino"]
+
+        # format_data(resources, money)
+
+        if user_choice == "espresso":
+            print(f"You have chosen: {espresso}")
+            water_level = resources["water"] - espresso["ingredients"]["water"]
+            milk_level = resources["milk"]  # - espresso["ingredients"]["milk"]
+            coffee_level = resources["coffee"]
+            choice = f"Water Level: {water_level}\nMilk Level: {milk_level}\nCoffee Level: {coffee_level}"
+
+        elif user_choice == "latte":
+            print(f"You have chosen: {latte}")
+            water_level = water - menu["latte"]["ingredients"]["water"]
+            milk_level = milk - menu["latte"]["ingredients"]["milk"]
+            coffee_level = coffee - menu["latte"]["ingredients"]["coffee"]
+            choice = f"Water Level: {water_level}\nMilk Level: {milk_level}\nCoffee Level: {coffee_level}"
+
+        elif user_choice == "cappuccino":
+            print(f"You have chosen: {cappuccino}")
+            water_level = water - menu["cappuccino"]["ingredients"]["water"]
+            milk_level = milk - menu["cappuccino"]["ingredients"]["milk"]
+            coffee_level = coffee - menu["cappuccino"]["ingredients"]["coffee"]
+            choice = f"Water Level: {water_level}\nMilk Level: {milk_level}\nCoffee Level: {coffee_level}"
+
+        return choice
+
     # TODO: 1 Prompt user by asking "What would you like? (espresso/latte/cappuccino):"
     user_choice = input(
         "What would you like? (espresso/latte/cappuccino):"
@@ -37,18 +67,13 @@ while machine_on:
     # 3. Print report.
     elif user_choice == "report":
         print(format_data(resources, money))
+    # TODO: 4 Check resources sufficient?
+    else:
+        check_resources(user_choice)
 
-# TODO: 4 Check resources sufficient?
 
 # TODO: 5 Process coins.
 
 # TODO: 6 Check transaction successful?
 
 # TODO: 7 Make Coffee.
-=======
-from data import menu, resources
-
-MENU = menu
-
-print(f"{MENU}")
->>>>>>> origin/master
